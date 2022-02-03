17d9d9b6a5c156894c7fe1702905803bd921a2e3
Dagi <dagi.zewdu.dz@gmail.com>
2022-02-03 21:09:45 +0300

    Added the whole functinality of the webiste

---
+++ assets/js/developer.data.js
@@ -0,0 +1,46 @@
+const Developers = [
+  {
+    id: 123,
+    username: 'Hany soliman',
+    image: 'assets/images/HanySoliman.jpeg',
+    position: 'Founder of Matrix tech',
+    quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  },
+  {
+    id: 113,
+    username: 'Dagic zewdu',
+    image: 'https://avatars.githubusercontent.com/u/68595873?v=4',
+    position: 'Founder & CTO',
+    quote: 'First, solve the problem. Then, write the code.',
+  },
+  {
+    id: 143,
+    username: 'Sutasit Srivisarvacha',
+    image: 'https://gojilabs.com/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-9.55.27-PM-400x388.png',
+    position: 'Product Manager',
+    quote: 'Experience is the name everyone gives to their mistakes',
+  },
+  {
+    id: 523,
+    username: 'Birgitte Hellsten',
+    image: 'https://gojilabs.com/wp-content/uploads/2020/09/Birgitte-Hellsten-400x400.jpeg',
+    position: 'Project Manager',
+    quote: 'In order to be irreplaceable, one must always be different',
+  },
+  {
+    id: 333,
+    username: 'Phil Davies',
+    image: 'https://gojilabs.com/wp-content/uploads/2021/01/Phil-Davies-e1615570808511-400x400.jpg',
+    position: 'Fullstack developer',
+    quote: 'Java is to JavaScript what car is to Carpet.',
+  },
+  {
+    id: 123,
+    username: 'Marta Maltseva',
+    image: 'https://gojilabs.com/wp-content/uploads/2021/07/Marta-Maltseva-scaled-e1627538800604-400x400.jpg',
+    position: 'Android developer',
+    quote: 'Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.',
+  },
+];
+
+export default Developers;
